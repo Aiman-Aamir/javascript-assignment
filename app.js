4ae0955d0848d7fbe651d6e19a943530ec3498c6
Aiman-Aamir <aymanbatool2015@gmail.com>
2023-04-12 03:07:22 +0500

    new questions added

--- app.js
+++ app.js
@@ -50,52 +50,58 @@
 
 //Q4...
 
-localStorage.clear()
+// localStorage.clear()
 
-let object_1 = {
-    name:"Aiman",
-    age: 20,
-    phone_no:"0343385678",
-    height:"5'3",
-    cnic: "433678901234",
-    eng_marks:78,
-    urdu_marks:45,
-    maths_marks:78
-}
-
-
-let object_2 = {
-    name:"Aliza",
-    age: 17,
-    phone_no:"96789453",
-    height:"4'8",
-    cnic: "8058543456-9",
-    eng_marks:45,
-    urdu_marks:67,
-    maths_marks:63
-}
+// let object_1 = {
+//     name:"Aiman",
+//     age: 20,
+//     phone_no:"0343385678",
+//     height:"5'3",
+//     cnic: "433678901234",
+//     eng_marks:78,
+//     urdu_marks:45,
+//     maths_marks:78
+// }
 
 
-let object_3 = {
-    name:"Ramsha",
-    age: 21,
-    phone_no:"567890432",
-    height:"5'7",
-    cnic: "8353468975",
-    eng_marks:74,
-    urdu_marks:50,
-    maths_marks:90
-}
-
-function object_saving(key , value){
-    let new_object = JSON.stringify(value);
-    localStorage.setItem(key, new_object);
-}
+// let object_2 = {
+//     name:"Aliza",
+//     age: 17,
+//     phone_no:"96789453",
+//     height:"4'8",
+//     cnic: "8058543456-9",
+//     eng_marks:45,
+//     urdu_marks:67,
+//     maths_marks:63
+// }
 
 
-object_saving("data of Aiman" , object_1);
-object_saving("data of Aliza", object_2 );
-object_saving("data of Ramsha", object_3 );
-object_saving("data of Aiman" , object_1);
-object_saving("data of Aliza", object_2 );
-object_saving("data of Ramsha", object_3 );
+// let object_3 = {
+//     name:"Ramsha",
+//     age: 21,
+//     phone_no:"567890432",
+//     height:"5'7",
+//     cnic: "8353468975",
+//     eng_marks:74,
+//     urdu_marks:50,
+//     maths_marks:90
+// }
+
+// function object_saving(key , value){
+//     let new_object = JSON.stringify(value);
+//     localStorage.setItem(key, new_object);
+// }
+
+
+// object_saving("data of Aiman" , object_1);
+// object_saving("data of Aliza", object_2 );
+// object_saving("data of Ramsha", object_3 );
+// object_saving("data of Aiman" , object_1);
+// object_saving("data of Aliza", object_2 );
+// object_saving("data of Ramsha", object_3 );
+
+// Q5
+// function changeColor(element, color) {
+//     element.style.backgroundColor = color;
+//   }
+  
